b921ce127ba8b69574187a08755eaf7c3fe0c0a6
Leo-Turible <turibleleo@gmail.com>
2024-01-08 02:10:14 +0100

    New components made to simplify the slider experimentation

---
+++ src/components/RealisationCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: String,
+  description: String,
+  image: String
+})
+</script>
+
+<template>
+  <div class="card-realisation snap-center flex flex-col items-center justify-start h-auto max-w-72 w-full min-h-96 bg-slate-300 p-1.5 rounded-xl flex-shrink-0">
+    <div class="card-realisation__img max-w-full">
+      <img :src="image" alt="" class="rounded">
+    </div>
+    <div class="card-realisation__content ">
+      <h3 class="card-realisation__title text-center">{{ title }}</h3>
+      <p class="card-realisation__description text-justify">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RealisationSlider.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue'
+import RealisationCard from './RealisationCard.vue'
+
+defineProps({
+  cards: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div id="slider"
+       class="flex flex-nowrap items-start justify-start gap-16 w-[80%] overflow-x-scroll snap-x snap-mandatory">
+    <div class="w-1/2 flex-shrink-0"></div>
+
+    <RealisationCard
+        v-for="card in cards"
+        :key="card.id"
+        :title="card.title"
+        :description="card.description"
+        :image="card.image"
+    />
+
+    <div class="w-1/2 flex-shrink-0"></div>
+  </div>
+</template>
+
+<style scoped>
+#slider::-webkit-scrollbar {
+  display: none;
+}
+
+#slider {
+  scrollbar-width: none;
+}
+</style>
