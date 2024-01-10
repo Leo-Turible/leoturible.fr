b8b788162a9bd6304c5002c5a7dc4c45b444120d
Leo-Turible <turibleleo@gmail.com>
2024-01-10 16:30:56 +0100

    fields that empty automatically + success or failure message

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -26,6 +26,12 @@ const sendEmail = (e) => {
         console.log(result.text);
         console.log(form.value);
         formStatus.value = 'Email sent successfully!';
+        // Réinitialiser les champs du formulaire
+        firstName.value = '';
+        lastName.value = '';
+        phone.value = '';
+        email.value = '';
+        message.value = '';
       }, (error) => {
         console.log(error.text);
         console.log(form.value);
@@ -89,6 +95,7 @@ onMounted(() => {
                   class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Écrivez vos pensées ici..."></textarea>
       </div>
+      <p>{{ formStatus }}</p>
       <button type="submit"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Envoyer
