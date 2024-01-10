0929064c4dfe0c0ee1cbd958bc6a3a984f00b88a
Leo-Turible <turibleleo@gmail.com>
2024-01-10 20:39:41 +0100

    last section added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -92,19 +92,33 @@ onMounted(() => {
       </h2>
     </section>
 
-    <section id="presentation" class="fade-in h-screen w-full flex flex-col items-center justify-around snap-center">
+    <section id="presentation"
+             class="fade-in h-screen w-full flex flex-col items-center justify-around snap-center px-24">
       <div class="w-full text-center self-start">
         <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
           Présentation</h2>
       </div>
       <div class="flex flex-col sm:flex-row flex-wrap items-center justify-center w-full">
-        <div class="flex items-center justify-center sm:flex-1">
+        <div class="flex items-center justify-center sm:flex-1 sm:basis-1/3">
           <img src="/fallout.webp" alt="Photo de profil" class="w-48 h-48 rounded-full object-cover mb-4 sm:mb-0">
         </div>
-        <div class="flex items-center justify-center sm:flex-1">
-          <p class="text-gray-900 dark:text-white text-justify w-2/3">
-            Bonjour, je suis Léo Turible, un étudiant en développement Full-Stack. J'ai une passion pour la création de
-            solutions logicielles efficaces et l'apprentissage de nouvelles technologies.
+        <div
+            class="flex flex-col items-center justify-center sm:flex-1 gap-5 sm:basis-2/3 px-10 py-5 bg-slate-100 rounded-xl shadow-xl">
+          <p class="text-gray-900 dark:text-white text-justify">
+            Je suis Léo Turible, j'ai 20 ans, et je suis en 2ème année de mon BUT Métier du Multimédia et de l'Internet
+            à Troyes. Depuis mon plus jeune âge, je suis passionné par l'informatique et l'innovation technologique.
+            Aujourd'hui, je suis déterminé à travailler dans le domaine de la programmation et du développement web.
+          </p>
+
+          <p class="text-gray-900 dark:text-white text-justify">
+            En tant que développeur web en herbe, je suis constamment à la recherche de nouveaux défis et de projets
+            intéressants qui me permettront d'améliorer mes compétences et de développer mes connaissances.
+          </p>
+
+          <p class="text-gray-900 dark:text-white text-justify">
+            Dans le cadre de ma formation, je suis à la recherche d'une entreprise qui pourrait m'offrir une alternance
+            en développement web pour la rentrée de septembre 2024. Cette opportunité sera cruciale pour ma poursuite
+            d'études et pour mon développement professionnel.
           </p>
         </div>
       </div>
@@ -115,7 +129,8 @@ onMounted(() => {
       <RealisationSlider :cards="cards"/>
     </section>
 
-    <section id="competence" class="fade-in h-screen w-full flex flex-col items-center justify-center snap-center">
+    <section id="competence"
+             class="fade-in h-screen w-full flex flex-col items-center justify-center snap-center gap-5">
       <h2 class="text-4xl font-bold dark:text-white">Compétences</h2>
       <SkillsComponent/>
     </section>
@@ -128,6 +143,24 @@ onMounted(() => {
       <ContactForm/>
     </section>
 
+    <section class="fade-in h-screen w-full flex flex-col items-center justify-between snap-center">
+      <div class=" w-full text-center self-start ">
+        <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+          Réseaux sociaux</h2>
+        <div class="flex flex-col items-center justify-center gap-5">
+          <a href="https://www.linkedin.com/in/leo-turible/" target="_blank"
+             class="px-10 py-2.5 m-3.5 bg-indigo-500 text-white rounded-xl transition ease-in-out hover:bg-indigo-300 hover:scale-110">LinkedIn</a>
+          <a href="https://github.com/Leo-Turible" target="_blank"
+             class="px-10 py-2.5 m-3.5 bg-indigo-500 text-white rounded-xl transition ease-in-out hover:bg-indigo-300 hover:scale-110">GitHub</a>
+          <a href="mailto:turibleleo@gmail.com"
+             class="px-10 py-2.5 m-3.5 bg-indigo-500 text-white rounded-xl transition ease-in-out hover:bg-indigo-300 hover:scale-110">Envoyez-moi
+            un email</a>
+        </div>
+      </div>
+      <footer class="h-10 w-full flex items-center justify-center bg-slate-500 dark:bg-gray-800">
+        <p class="text-gray-900 dark:text-white">© 2021 Léo Turible. Tous droits réservés.</p>
+      </footer>
+    </section>
   </main>
 </template>
 
