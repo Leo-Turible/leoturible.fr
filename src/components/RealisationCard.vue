83367179b9b618868102d5521cc84c629a7a05c2
Leo-Turible <turibleleo@gmail.com>
2024-01-11 16:57:56 +0100

    css update

--- src/components/RealisationCard.vue
+++ src/components/RealisationCard.vue
@@ -15,12 +15,12 @@ defineProps({
     <div class="card-realisation__img max-w-full">
       <img :src="image" alt="" class="">
     </div>
-    <div class="card-realisation__content flex items-stretch justify-center flex-col">
+    <div class="card-realisation__content flex items-stretch justify-start flex-col h-full">
       <h3 class="card-realisation__title text-center border-slate-900 border-y-2  py-0.5 text-3xl font-bold dark:text-white">
         {{ title }}</h3>
       <p class="card-realisation__description text-justify p-3.5 text-gray-500">{{ description }}</p>
       <a :href="url"
-         class="card-realisation__discover-link self-center px-10 py-2.5 m-3.5 bg-indigo-500 text-white rounded-xl transition ease-in-out hover:bg-indigo-300 hover:scale-110">Découvrir</a>
+         class="card-realisation__discover-link self-center px-10 py-2.5 m-3.5 bg-indigo-500 text-white rounded-xl transition ease-in-out hover:bg-indigo-300 hover:scale-110 mt-auto">Découvrir</a>
     </div>
   </div>
 </template>
