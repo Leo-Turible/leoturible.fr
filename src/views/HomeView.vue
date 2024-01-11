dacee514b149de793e86e88ed04e019ca0749b55
Leo-Turible <turibleleo@gmail.com>
2024-01-11 17:51:56 +0100

    h3 fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -84,10 +84,10 @@ onMounted(() => {
   <main class="snap-y snap-mandatory h-screen overflow-x-hidden">
 
     <section id="about" class="fade-in h-screen w-full flex flex-col items-center justify-center snap-start">
-      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl ">
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900  md:text-5xl lg:text-6xl ">
         <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-300 from-sky-400">Léo Turible</span>
       </h1>
-      <h2 class="text-4xl font-bold dark:text-white text-center">
+      <h2 class="text-4xl font-bold  text-center">
         Full-Stack Developer Student
       </h2>
     </section>
@@ -95,7 +95,7 @@ onMounted(() => {
     <section id="presentation"
              class="fade-in h-screen w-full flex flex-col items-center justify-around snap-center px-4 sm:px-8 md:px-24">
       <div class="w-full text-center self-start">
-        <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+        <h2 class="mb-4 text-3xl font-bold text-gray-900  md:text-4xl text-center">
           Présentation</h2>
       </div>
       <div class="flex flex-col sm:flex-row flex-wrap items-center justify-center w-full">
@@ -104,18 +104,18 @@ onMounted(() => {
         </div>
         <div
             class="flex flex-col items-center justify-center sm:flex-1 gap-5 sm:basis-2/3 px-2 sm:px-10 py-5 bg-slate-100 rounded-xl shadow-xl">
-          <p class="text-gray-900 dark:text-white text-justify">
+          <p class="text-gray-900  text-justify">
             Je suis Léo Turible, j'ai 20 ans, et je suis en 2ème année de mon BUT Métier du Multimédia et de l'Internet
             à Troyes. Depuis mon plus jeune âge, je suis passionné par l'informatique et l'innovation technologique.
             Aujourd'hui, je suis déterminé à travailler dans le domaine de la programmation et du développement web.
           </p>
 
-          <p class="text-gray-900 dark:text-white text-justify">
+          <p class="text-gray-900  text-justify">
             En tant que développeur web en herbe, je suis constamment à la recherche de nouveaux défis et de projets
             intéressants qui me permettront d'améliorer mes compétences et de développer mes connaissances.
           </p>
 
-          <p class="text-gray-900 dark:text-white text-justify">
+          <p class="text-gray-900  text-justify">
             Dans le cadre de ma formation, je suis à la recherche d'une entreprise qui pourrait m'offrir une alternance
             en développement web pour la rentrée de septembre 2024. Cette opportunité sera cruciale pour ma poursuite
             d'études et pour mon développement professionnel.
@@ -125,19 +125,19 @@ onMounted(() => {
     </section>
 
     <section id="realisation" class="fade-in h-screen w-full flex flex-col items-center justify-evenly snap-center">
-      <h2 class="text-4xl font-bold dark:text-white">Réalisations</h2>
+      <h2 class="text-4xl font-bold ">Réalisations</h2>
       <RealisationSlider :cards="cards"/>
     </section>
 
     <section id="competence"
              class="fade-in h-screen w-full flex flex-col items-center justify-center snap-center gap-5">
-      <h2 class="text-4xl font-bold dark:text-white">Compétences</h2>
+      <h2 class="text-4xl font-bold ">Compétences</h2>
       <SkillsComponent/>
     </section>
 
     <section id="contact" class="fade-in h-screen w-full flex flex-col items-center justify-around snap-center">
       <div class="w-full text-center self-start">
-        <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+        <h2 class="mb-4 text-3xl font-bold text-gray-900  md:text-4xl text-center">
           Contact</h2>
       </div>
       <ContactForm/>
@@ -145,7 +145,7 @@ onMounted(() => {
 
     <section class="relative fade-in h-screen w-full flex flex-col items-center justify-center snap-center">
       <div class="h-full w-full text-center flex flex-col items-center justify-start gap-28">
-        <h2 class="mt-8 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+        <h2 class="mt-8 mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl text-center">
           Réseaux sociaux</h2>
         <div class="flex flex-col items-center justify-center gap-5">
           <a href="https://www.linkedin.com/in/leoturible/" target="_blank"
@@ -160,8 +160,8 @@ onMounted(() => {
           </a>
         </div>
       </div>
-      <footer class="absolute bottom-0 h-10 w-full opacity-75 flex items-center justify-center bg-slate-500 dark:bg-gray-800">
-        <p class="text-gray-100 dark:text-white">© 2021 Léo Turible. Tous droits réservés.</p>
+      <footer class="absolute bottom-0 h-10 w-full opacity-75 flex items-center justify-center bg-slate-500">
+        <p class="text-gray-100 ">© 2021 Léo Turible. Tous droits réservés.</p>
       </footer>
     </section>
   </main>
